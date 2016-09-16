9cfd89e75cae573830de3f1c4af123bcd360d088
1143849893 <1143849893@sc309e20.academico.icesi.edu.co>
2016-09-16 15:29:46 -0500

    Creacion de la clase Logica y MainApp

--- ejercicioDCA_semana7/src/ejercicioDCA_semana7/MainApp.java
+++ ejercicioDCA_semana7/src/ejercicioDCA_semana7/MainApp.java
@@ -1,5 +1,33 @@
 package ejercicioDCA_semana7;
 
+import processing.core.PApplet;
+
 public class MainApp {
+
+	public static MainApp app;
+	Logica logica;
+
+	public static void main(String[] args) {
+
+		PApplet.main("MainApp");
+	}
+
+	public void setup() {
+
+		MainApp.app = this;
+		logica = new Logica();
+
+	}
+
+	public void settings() {
+		size(1200, 700);
+
+	}
+
+	public void draw() {
+		background(255);
+		
+
+	}
 
 }
